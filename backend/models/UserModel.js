16fb842f393f044fe766d262310cf1f6b5002f42
anuj <anuj@C40LAP-10090.india.rsystems.com>
2022-12-14 21:37:32 +0530

    mern-tnn-02 - email and password validation with validator plugin.

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -17,6 +18,19 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is invalid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) {
