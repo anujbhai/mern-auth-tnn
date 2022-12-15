df05786462fca9e0fb8518b2f7f88bb89d0fda64
anuj <anuj@C40LAP-10090.india.rsystems.com>
2022-12-15 21:29:37 +0530

    mern-auth-tnn-05 - auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WorkoutsContextProvider } from "./context/WorkoutsContext";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
-      <App />
-    </WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <App />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
