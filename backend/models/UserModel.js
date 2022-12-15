70725101a6c8a036dc647e05864249ebf67005b7
anuj <anuj@C40LAP-10090.india.rsystems.com>
2022-12-15 20:19:10 +0530

    mern-auth-tnn-04 - user login

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -44,4 +44,26 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+// static login method
+userSchema.statics.login = async function(email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({email});
+
+  if (!user) {
+    throw Error('User with the email not found. Please check your email.');
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+
+  return user;
+}
+
 module.exports = mongoose.model("User", userSchema);
