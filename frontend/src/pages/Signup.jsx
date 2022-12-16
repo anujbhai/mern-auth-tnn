749e15f0524626258b43c640020ef5d8766316d6
anuj <anuj@C40LAP-10090.india.rsystems.com>
2022-12-16 21:26:42 +0530

    mern-auth-tnn-07 - useSignup hook

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,13 +1,16 @@
 import {useState} from "react";
+import { useSignup} from "../hooks/useSignup.jsx";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const {signup, error, isLoading} = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     console.log("form submitted for login with email", email);
+    await signup(email, password);
   };
 
   return (
@@ -28,7 +31,10 @@ const Signup = () => {
         value={password}
       />
 
-      <button>Sign up</button>
+      <button disabled={isLoading}>Sign up</button>
+      {error && <div className="error">
+        {error}
+      </div>}
     </form>
   );
 };
